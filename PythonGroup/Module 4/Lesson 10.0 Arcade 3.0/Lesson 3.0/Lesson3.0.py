2dfce0fb89487a236dfaeb1cd3b0f9ae1478c047
Matrixko <matvey.masko@gmail.com>
2022-05-15 12:26:20 +0300

    yes

--- PythonGroup/Module 4/Lesson 10.0 Arcade 3.0/Lesson 3.0/Lesson3.0.py
+++ PythonGroup/Module 4/Lesson 10.0 Arcade 3.0/Lesson 3.0/Lesson3.0.py
@@ -30,6 +30,7 @@ C_GREEN = (32, 128, 32)
 C_RED = (255, 0, 0)
 C_BLACK = (0, 0, 0)
 
+
 # Классы
 # класс для цели (стоит и ничего не делает)
 class FinalSprite(pygame.sprite.Sprite):
@@ -131,16 +132,57 @@ class Enemy(pygame.sprite.Sprite): # враг
         self.rect.y += randint(-5, 5)
 
 # Запуск игры 
+pygame.display.set_caption("ARCADA")
+window = pygame.display.set_mode([win_width, win_height])
+back = pygame.transform.scale(pygame.image.load(img_file_back).convert(), (win_width, win_height))
 
 
 # список всех персонажей игры:
+all_sprites = pygame.sprite.Sprite.Group()
+
+barriers = pygame.sprite.Group()
+
+enemies = pygame.sprite.Group()
+
+bombs = pygame.sprite.Group()
+
+# create a character, add it to the list of all sprites:
+robin = Hero(img_file_hero)
+all_sprites.add(robin)
+
+# create walls, add them:
+w = Wall(50, 150, 480, 20)
+barriers.add(w)
+all_sprites.add(w)
+w = Wall(700, 50, 50, 360)
+barriers.add(w)
+all_sprites.add(w)
+w = Wall(350, 380, 640, 20)
+barriers.add(w)
+all_sprites.add(w)
+w = Wall(-200, 590, 1600, 20)
+barriers.add(w)
+all_sprites.add(w)
 
 
 # список препятствий:
 
-# список врагов:
+en = Enemy(50, 480)
+all_sprites.add(en)
+enemies.add(en)
 
-# список мин:
+en = Enemy(400, 480)
+all_sprites.add(en)
+enemies.add(en)
+
+
+bomb = Enemy(550, 520, img_file_bomb, 60, 60)
+bombs.add(bomb)
+
+pr = FinalSprite(img_file_princess, win_width + 500, win_height - 150, 0)
+all_sprites.add(pr)
+
+
 
 
 # создаем персонажа, добавляем его в список всех спрайтов:
@@ -150,58 +192,84 @@ class Enemy(pygame.sprite.Sprite): # враг
 
 
 
-# создаем врагов, добавляем их:
+# Main game loop:
+run = True
+finished = False
 
+while run:
+   # Event Handling
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           run = False
+       elif event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_LEFT:
+               robin.x_speed = -5
+           elif event.key == pygame.K_RIGHT:
+               robin.x_speed = 5
+           elif event.key == pygame.K_UP:
+               robin.jump(-7)
 
-# создаем мины, добавляем их:
-            
-            # в список всех спрайтов бомбы не добавляем, будем рисовать их отдельной командой
-            # так легко сможем подрывать бомбы, а также делаем их неподвижными, update() не вызывается
+       elif event.type == pygame.KEYUP:
+           if event.key == pygame.K_LEFT:
+               robin.x_speed = 0
+           elif event.key == pygame.K_RIGHT:
+               robin.x_speed = 0
 
-# создаем финальный спрайт, добавляем его: 
+     if not finished:
+       # Moving Game Objects
+       all_sprites.update()
 
+       # beyond checking the rules of the game
+       # check touch with bombs:
+       pygame.sprite.groupcollide(bombs, all_sprites, True, True)
+               # if the bomb touches the sprite, then it is removed from the list of bombs, and the sprite is removed from all_sprites!
 
+       # check the touch of the hero with the enemies:
+       if pygame.sprite.spritecollide(robin, enemies, False):
+           robin.kill() # the kill method removes the sprite from all groups in which it is listed
 
+       # check screen borders:
+       if (
+           robin.rect.x > right_bound and robin.x_speed > 0
+           or
+           robin.rect.x < left_bound and robin.x_speed < 0
+       ): # when exiting to the left or right, we transfer the change to the screen shift
+           shift -= robin.x_speed 
+           # move all the sprites to a common shift (and separately the bombs, they are in another list):
+           for s in all_sprites:
+               s.rect.x -= robin.x_speed # robin himself is also in this list, so his movement will be visually canceled
+           for s in bombs:
+               s.rect.x -= robin.x_speed
 
+       # draw the background with a shift
+       local_shift = shift % win_width
+       window.blit(back, (local_shift, 0))
+       if local_shift != 0:
+           window.blit(back, (local_shift - win_width, 0))
 
+       # draw all sprites on the screen surface before checking for win/loss
+       # if the game ended in this iteration of the loop, then a new background will be drawn on top of the characters
+       all_sprites.draw(window) 
+       # we draw a group of bombs separately - so a bomb that has left its group will automatically cease to be visible
+       bombs.draw(window)
 
-# Основной цикл игры: 
- 
-    # Обработка событий
-      
-        # Перемещение игровых объектов  
+       # check for win and loss:
+       if pygame.sprite.collide_rect(robin, pr):
+           finished = True
+           window.fill(C_BLACK)
+           # write text on the screen
+           text = font.render("YOU WIN!", 1, C_RED)
+           window.blit(text, (250, 250))
 
-        # дальше проверки правил игры
-        # проверяем касание с бомбами: 
-                # если бомба коснулась спрайта, то она убирается из списка бомб, а спрайт - из all_sprites!
+       # check for loss:
+       if robin not in all_sprites or robin.rect.top > win_height:
+           finished = True           
+           window.fill(C_BLACK)
+           # write text on the screen
+           text = font.render("GAME OVER", 1, C_RED)
+           window.blit(text, (250, 250))
 
-        # проверяем касание героя с врагами: 
-           # robin.kill() # метод kill убирает спрайт из всех групп, в которых он числится
+   pygame.display.update()
 
-        # проверяем границы экрана: 
-             # при выходе влево или вправо переносим изменение в сдвиг экрана 
-            # перемещаем на общий сдвиг все спрайты (и отдельно бомбы, они ж в другом списке): 
-                        # сам robin тоже в этом списке, поэтому его перемещение визуально отменится
-            
-
-        # Отрисовка
-        # рисуем фон со сдвигом
-        
-
-        # нарисуем все спрайты на экранной поверхности до проверки на выигрыш/проигрыш
-        # если в этой итерации цикла игра закончилась, то новый фон отрисуется поверх персонажей
-         
-        # группу бомб рисуем отдельно - так бомба, которая ушла из своей группы, автоматически перестанет быть видимой
-       
-
-        # проверка на выигрыш и на проигрыш:
-        
-
-        # проверка на проигрыш:
-         
-            # пишем текст на экране
-             
-
-     
-
-    # Пауза 
+   # Pause
+   pygame.time.delay(20)
