53a5f9ed59531d4c0024f810a354fd43a05c70a9
alkihuri <alkihuri@gmail.com>
2022-05-28 20:28:18 +0300

    Update PyQt 2.0.py

--- PythonGroup/Module 4/PyQt 2.0/PyQt 2.0.py
+++ PythonGroup/Module 4/PyQt 2.0/PyQt 2.0.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QLabel, QLineEdit
 import sys
 
 
@@ -8,27 +8,72 @@ class MyWindow(QMainWindow):
         super(MyWindow,self).__init__() 
         self.counter = 0 
         self.setGeometry(200,200,300,300)
-        self.setWindowTitle("Counter")
-        self.CreateLabel("0",100,100) 
-        self.CreateButton("Click!",100,150,self.CounterFunc)
+        self.setWindowTitle("Calculator")
 
-    def CreateLabel(self,text,x=50,y=50):
-        self.newLabel = QtWidgets.QLabel(self)
-        self.newLabel.setText(text)
-        self.newLabel.move(x,y) 
+        self.aField         = self.CreateInputField("a:",100,25)
+        self.bField         = self.CreateInputField("b:",100,50)
+        self.resultField    = self.CreateLabel("Result:",50,100)
+
+        self.plusBtn        = self.CreateButton("+",100,150,self.Plus)
+        self.minusBtn       = self.CreateButton("-",100,175,self.Minus)
+        self.multiplyBtn    = self.CreateButton("*",100,200,self.Multiply)
+        self.divideBtn      = self.CreateButton("/",100,225,self.Divide)
+
+        self.a = 0
+        self.b = 0 
+        self.result = 0 
+
+    def CreateLabel(self,text,x,y):
+        newLabel = QtWidgets.QLabel(self)
+        newLabel.setText(text)
+        newLabel.move(x,y)
+        return newLabel
+    
+    def CreateInputField(self,label,x,y):
+        self.CreateLabel(label,x-50,y)
+        line = QLineEdit(self) 
+        line.move(x, y)
+        line.resize(200, 32)
+        return line
 
     def CreateButton(self,text,x,y,fun):
-        self.newButton = QtWidgets.QPushButton(self)
-        self.newButton.setText(text)
-        self.newButton.move(x,y)
-        self.newButton.clicked.connect(fun)
+        newButton = QtWidgets.QPushButton(self)
+        newButton.setText(text)
+        newButton.move(x,y)
+        newButton.clicked.connect(fun) 
+        return newButton
 
-    def CounterFunc(self):
-        self.counter += 1 
-        self.newButton.adjustSize()
-        self.newLabel.adjustSize()
-        self.newLabel.setText(str(self.counter))
-        self.update() 
+    def Update(self):
+        try:
+            self.a =  float(self.aField.text())
+        except:
+            self.a = 0
+        try:
+            self.b =  float(self.bField.text())
+        except:
+            self.b = 0
+        self.resultField.setText("Result : " + str(self.result))
+
+
+    def Plus(self): 
+        self.Update()
+        self.result =  self.a + self.b
+        self.Update()
+
+    def Minus(self): 
+        self.Update()
+        self.result = self.a - self.b
+        self.Update()
+
+    def Multiply(self): 
+        self.Update()
+        self.result = self.a * self.b
+        self.Update()
+
+    def Divide(self): 
+        self.Update()
+        self.result = self.a / self.b
+        self.Update()
 
 app = QApplication(sys.argv)
 window  = MyWindow() 
