84bfa0e8402db3459eb58918b43dc52c293104c3
alkihuri <alkihuri@gmail.com>
2022-06-12 01:50:47 +0300

    Update PyQt 4.0.py

--- PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
+++ PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
@@ -1,12 +1,10 @@
 #PyQt 4.0 
 #work with files READ and WRITE
 #presentation link https://docs.google.com/presentation/d/1bdxF5J2j9vmLCPbtVkgYI_0DsI1r05Yv_gwBHwv0ypk/edit?usp=sharing
-
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication,QMainWindow, QWidget, QLabel, QLineEdit,QComboBox 
+from PyQt5.QtWidgets import QApplication,QMainWindow,QComboBox 
 import sys
-import os
- 
+import os 
 
 class ReaderView(QMainWindow):  
 
@@ -15,10 +13,9 @@ class ReaderView(QMainWindow):
         self.myViewModel = myViewModel                       
         self.setGeometry(0,0,600,600)                                        
         self.setWindowTitle("Poem reader") 
-        self.poemSelectField              = self.CreateComboBox("Poem:",    150,25)     
+        self.poemSelectField = self.CreateComboBox("Poem:",    150,25)     
         self.poemSelectField.addItems(["WILLIAM SHAKESPEARE","RUDYARD KIPLING"])
-        self.poemField              = self.CreateLabel("POEM NOT LOADED",150,100)
-        self.getPoembtn             = self.CreateButton("Get poem",      150,500, self.RequestPoem) 
+        self.poemField = self.CreateLabel("POEM NOT LOADED",150,100) 
 
     def RequestPoem(self):
         self.poemString = self.myViewModel.SetPath(self.poemSelectField.currentText())   
@@ -30,24 +27,15 @@ class ReaderView(QMainWindow):
         newLabel.move(x,y)
         return newLabel
 
-    def CreateComboBox(self,label,x,y):
-        offsetForInputField = 50
+    def CreateComboBox(self,label,x,y): 
         self.CreateLabel(label,x,y)
         combo = QComboBox(self) 
-        combo.move(x+offsetForInputField, y)
+        combo.move(x+50, y)
         combo.resize(200, 32)
-        return combo
+        combo.activated[str].connect(self.RequestPoem)      
+        return combo 
 
-    def CreateButton(self,text,x,y,fun):
-        newButton = QtWidgets.QPushButton(self)
-        newButton.setText(text)
-        newButton.resize(250, 50)
-        newButton.move(x,y)
-        newButton.clicked.connect(fun) 
-        return newButton
-
-    def UpdateUI(self):
-        self.poemField.setText("")
+    def UpdateUI(self): 
         total = ""
         lineOffset = 25 
         for x in self.poemString: 
@@ -72,6 +60,7 @@ class ReaderModel():
         for x in f:
             poem.append(x)
         return poem
+
 
 
 app = QApplication(sys.argv)  
