bfaa8712f3f549018e6257b3ce730b2d056a6a46
alkihuri <alkihuri@gmail.com>
2022-06-12 01:06:49 +0300

    Template

--- PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
+++ PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
@@ -2,4 +2,15 @@
 #work with files READ and WRITE
 #presentation link https://docs.google.com/presentation/d/1bdxF5J2j9vmLCPbtVkgYI_0DsI1r05Yv_gwBHwv0ypk/edit?usp=sharing
 
- 
+
+import os
+poem = list()
+currentFilePath     = os.path.dirname(__file__)                  
+poemFilePath = currentFilePath + "/RUDYARD KIPLING.txt"
+f = open(poemFilePath, "r")
+for x in f:
+    print(x)
+
+newpoemFilePath = currentFilePath + "/WILLIAM SHAKESPEARE.txt"
+f = open(newpoemFilePath,"w")
+
