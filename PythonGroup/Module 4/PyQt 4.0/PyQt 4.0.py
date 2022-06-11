2a312662195523fad840144e83cb5e1163928147
alkihuri <alkihuri@gmail.com>
2022-06-12 02:07:32 +0300

    Update PyQt 4.0.py

--- PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
+++ PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
@@ -3,14 +3,7 @@
 #presentation link https://docs.google.com/presentation/d/1bdxF5J2j9vmLCPbtVkgYI_0DsI1r05Yv_gwBHwv0ypk/edit?usp=sharing
 
 
-import os
-poem = list()
-currentFilePath     = os.path.dirname(__file__)                  
-poemFilePath = currentFilePath + "/RUDYARD KIPLING.txt"
-f = open(poemFilePath, "r")
-for x in f:
-    print(x)
-
-newpoemFilePath = currentFilePath + "/WILLIAM SHAKESPEARE.txt"
-f = open(newpoemFilePath,"w")
-
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication,QMainWindow,QComboBox 
+import sys
+import os 
